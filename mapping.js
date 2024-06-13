f81e0af6af6cf9feaa0d7aed46e5e3db53284097
metablyy <metably@outlook.com>
2024-06-13 14:47:38 +0100

    almost functional player movement. no errors, however faulty code is clearly preventing the wanted outcome.

--- mapping.js
+++ mapping.js
@@ -14,30 +14,109 @@ image.onload = () => {
   c.drawImage(image, 0, 0)
 }
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-canvas.width = 1024
-canvas.height = 576
-
-console.log(charactersMap)
-
 const offset = {
   x: -735,
   y: -650
 }
 
+class Sprite {
+  constructor({
+    position,
+    velocity,
+    image,
+    frames = { max: 1, hold: 10 },
+    sprites,
+    animate = false,
+    rotation = 0,
+    scale = 1
+  }) {
+    this.position = position
+    this.image = new Image()
+    this.frames = { ...frames, val: 0, elapsed: 0 }
+    this.image.onload = () => {
+      this.width = (this.image.width / this.frames.max) * scale
+      this.height = this.image.height * scale
+    }
+    this.image.src = image.src
+
+    this.animate = animate
+    this.sprites = sprites
+    this.opacity = 1
+
+    this.rotation = rotation
+    this.scale = scale
+  }
+
+  draw() {
+    c.save()
+    c.translate(
+      this.position.x + this.width / 2,
+      this.position.y + this.height / 2
+    )
+    c.rotate(this.rotation)
+    c.translate(
+      -this.position.x - this.width / 2,
+      -this.position.y - this.height / 2
+    )
+    c.globalAlpha = this.opacity
+
+    const crop = {
+      position: {
+        x: this.frames.val * (this.width / this.scale),
+        y: 0
+      },
+      width: this.image.width / this.frames.max,
+      height: this.image.height
+    }
+
+    const image = {
+      position: {
+        x: this.position.x,
+        y: this.position.y
+      },
+      width: this.image.width / this.frames.max,
+      height: this.image.height
+    }
+
+    c.drawImage(
+      this.image,
+      crop.position.x,
+      crop.position.y,
+      crop.width,
+      crop.height,
+      image.position.x,
+      image.position.y,
+      image.width * this.scale,
+      image.height * this.scale
+    )
+
+    c.restore()
+
+    if (!this.animate) return
+
+    if (this.frames.max > 1) {
+      this.frames.elapsed++
+    }
+
+    if (this.frames.elapsed % this.frames.hold === 0) {
+      if (this.frames.val < this.frames.max - 1) this.frames.val++
+      else this.frames.val = 0
+    }
+  }
+}
+
+
 const playerDownImage = new Image()
-playerDownImage.src = './img/playerDown.png'
+playerDownImage.src = './assets/imgs/playerDown.png'
 
 const playerUpImage = new Image()
-playerUpImage.src = './img/playerUp.png'
+playerUpImage.src = './assets/imgs/playerUp.png'
 
 const playerLeftImage = new Image()
-playerLeftImage.src = './img/playerLeft.png'
+playerLeftImage.src = './assets/imgs/playerLeft.png'
 
 const playerRightImage = new Image()
-playerRightImage.src = './img/playerRight.png'
+playerRightImage.src = './assets/imgs/playerRight.png'
 
 const player = new Sprite({
   position: {
@@ -105,10 +184,6 @@ function animate() {
       ) {
         // deactivate current animation loop
         window.cancelAnimationFrame(animationId)
-
-        audio.Map.stop()
-        audio.initBattle.play()
-        audio.battle.play()
 
         battle.initiated = true
         gsap.to('#overlappingDiv', {
@@ -348,7 +423,6 @@ window.addEventListener('keyup', (e) => {
 let clicked = false
 addEventListener('click', () => {
   if (!clicked) {
-    audio.Map.play()
     clicked = true
   }
 })
